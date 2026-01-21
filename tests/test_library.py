9bfcfbed6fce37761c059336ebbf544581813de8
ANURAG KAR <anur240101094@iiitmanipur.ac.in>
2026-01-21 16:09:34 +0530

    [Sprint-2] feat: borrow and return book with validation

--- tests/test_library.py
+++ tests/test_library.py
@@ -14,3 +14,24 @@ class TestSprint1(unittest.TestCase):
         lib.add_book("B101", "Clean Code", "Robert Martin")
         with self.assertRaises(ValueError):
             lib.add_book("B101", "Clean Architecture", "Robert Martin")
+class TestSprint2(unittest.TestCase):
+
+    def test_borrow_available_book(self):
+        lib = Library()
+        lib.add_book("B201", "Refactoring", "Martin Fowler")
+        lib.borrow_book("B201")
+        self.assertEqual(lib.books["B201"]["status"], "Borrowed")
+
+    def test_borrow_unavailable_book(self):
+        lib = Library()
+        lib.add_book("B201", "Refactoring", "Martin Fowler")
+        lib.borrow_book("B201")
+        with self.assertRaises(ValueError):
+            lib.borrow_book("B201")
+
+    def test_return_book(self):
+        lib = Library()
+        lib.add_book("B201", "Refactoring", "Martin Fowler")
+        lib.borrow_book("B201")
+        lib.return_book("B201")
+        self.assertEqual(lib.books["B201"]["status"], "Available")
